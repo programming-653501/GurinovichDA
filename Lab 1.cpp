302e586fc29b65aa6d066924bfe8b02d1693526f
Gurinovich Dasha <dashagurinovich425@gmail.com>
2017-02-24 20:39:13 +0300

    Add files via upload

---
+++ Lab 1.cpp
@@ -0,0 +1,14 @@
+// ConsoleApplication1.cpp: определ€ет точку входа дл€ консольного приложени€.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	int k, sequance;
+	printf("¬ведите k:");
+
+	return 0;
+}
+
