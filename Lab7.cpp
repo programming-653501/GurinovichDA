3f6d650fc29bb967dcda5f2afcbbcecded26197c
Gurinovich Dasha <dashagurinovich425@gmail.com>
2017-04-26 23:36:24 +0300

    Add files via upload

---
+++ Lab7.cpp
@@ -0,0 +1,92 @@
+#pragma hdrstop
+#pragma argsused
+
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream.h>
+#include "List.h"
+
+int checkI(char *str);
+List depositors;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int count = 0, count1 = 0, x1 = 0, answer;
+	while (count1 == 0){
+		x1 = 0;
+		while (!x1){
+		char m1[3];
+			printf("\tPriorbank Menu:\n1.Account statement\n2.The list of all clients\n3.The search in the list of clients\n4.Sort by current deposit amount\n5.The sum of all balance\n6.Add client\n7.Delete client\n8.Exit\n");
+			fflush(stdin);
+			fgets(m1, 3, stdin);
+			answer = atoi(m1);
+			if (!checkI(m1)||(answer < 1 || answer > 8))
+				printf("\nEnter the right number!\n");
+			else {
+				x1 = 1;
+			}
+			if (answer == 1) {
+				char *personalAccount = new char [21];
+				cout<<"Enter the number of the account:";
+				gets(personalAccount);
+			}
+			else if (answer == 2) {
+				if (count == 0) {
+					cout<<"No clients in the list!\n";
+				}
+				else
+					depositors.Show();
+			}
+			else if (answer == 6) {
+				system("cls");
+				char *Name = new char [31];
+				char *personalAccount = new char [21];
+				char *passportNumber = new char [11];
+				char *depositType = new char [21];
+				double currentBalance;
+				printf("Enter the name of the client:");
+				gets(Name);
+				printf("Enter the number of personal account:");
+				gets(personalAccount);
+				printf("Enter the number of the passport:");
+				gets(passportNumber);
+				printf("Enter the type of the deposit:");
+				gets(depositType);
+				printf("Enter the current balance:");
+				cin>>currentBalance;
+				depositors.Add(Name, personalAccount, passportNumber, depositType, currentBalance);
+            	count++;
+			}
+			else if (answer == 7) {
+				system("cls");
+                if (count == 0) {
+					cout<<"No clients in the list!\n";
+				}
+				else {
+					char *Name = new char [31];
+					printf("Enter the name of the client who you want to delete:");
+					gets(Name);
+					depositors.Deleting(Name, count);
+				}
+			}
+			else if (answer == 8) {
+				count1 = 1;
+			}
+		}
+	}
+	return 0;
+}
+
+int checkI(char *str)
+{
+	int i, result = 0;
+	for (i = 0; i < (int) strlen(str); i++) {
+		if (isdigit(str[i])||str[i]=='\n') {
+			result = 1;
+		}
+		else {
+			result = 0;
+			break;}
+	}
+	return result;
+}
---
+++ List.cpp
@@ -0,0 +1,100 @@
+//---------------------------------------------------------------------------
+
+#pragma hdrstop
+
+#include "List.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+
+List::~List(){
+	Head = NULL;
+	while (First!=NULL){
+		ListItem *temp;
+		temp = First->next;
+		delete First;
+		First = temp;
+	}
+
+}
+
+void List::Add(char *Name, char *personalAccount, char *passportNumber, char *depositType, double currentBalance){
+	ListItem* item = new ListItem;
+	strcpy(item->name, Name);
+	strcpy(item->PersonalAccount, personalAccount);
+	strcpy(item->PassportNumber, passportNumber);
+	strcpy(item->DepositType, depositType);
+	item->CurrentBalance = currentBalance;
+	if (Head){
+		Head->next = item;
+		item->prev = Head;
+		item->next = NULL;
+		Head = item;
+	}
+	else{
+		item->next=NULL;
+		item->prev=NULL;
+		Head = item;
+		First = item;
+	}
+}
+
+void List::Show(){
+	ListItem *item;
+	item = First;
+	int j = 0;
+	while (item!=NULL){
+		printf("%s %s %s %s %f\n", item->name, item->PersonalAccount, item->PassportNumber, item->DepositType, item->CurrentBalance);
+		item = item->next;
+	}
+}
+
+
+void List::Deleting(char *deletingName, int &count){
+	ListItem *item;
+	item = First;
+	int c = 0;
+	while (item!=NULL){
+		if (strcmp(item->name,deletingName)==0) {
+			if (item->next) {
+				item->next->prev = item->prev;
+			}
+			else
+				Head = item->prev;
+			if (item->prev) {
+				item->prev->next = item->next;
+			}
+			else
+				First = item->next;
+			ListItem *temp;
+			temp = item->next;
+			delete item;
+			item = temp;
+			c = 1;
+		}
+		else
+			item = item->next;
+	}
+	if (c == 0) {
+		printf("The depositor hasn't been found!\n");
+	}
+	else
+		count--;
+}
+
+void List::Search(char *searchingName){
+	int c = 0;
+	ListItem *item;
+	item = First;
+	while (item!=NULL){
+		if (strcmp(item->name,searchingName)==0) {
+            printf("%s %s %s %s %f\n", item->name, item->PersonalAccount, item->PassportNumber, item->DepositType, item->CurrentBalance);
+			c = 1;
+		}
+		item = item->next;
+	}
+	if (c == 0) {
+		printf("The employee hasn't been found!\n");
+	}
+}
+
+
